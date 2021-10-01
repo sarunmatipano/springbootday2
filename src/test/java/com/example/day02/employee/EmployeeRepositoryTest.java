684891c4a89927067587f29e68a6ade3c8c94396
Sarun Matipano <sarun.m@cdg.co.th>
2021-10-01 16:41:15 +0700

    add test repository

--- src/test/java/com/example/day02/employee/EmployeeRepositoryTest.java
+++ src/test/java/com/example/day02/employee/EmployeeRepositoryTest.java
@@ -1,4 +1,34 @@
+package com.example.day02.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 class EmployeeRepositoryTest {
-  
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Test
+    public void case01(){
+        // arrange
+        Employee employee = new Employee();
+        employee.setName("Sarun");
+        repository.save(employee);
+
+        // act
+        Optional<Employee> result = repository.findById(1);
+
+        // assert
+        assertTrue(result.isPresent());
+        assertEquals(1, result.get().getId());
+        assertEquals("Sarun", result.get().getName());
+
+    }
+
 }
